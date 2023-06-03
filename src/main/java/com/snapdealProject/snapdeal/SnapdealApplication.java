7a54c33dec272f3f00202a8d7c78668f90e5cfa0
snapdeal12345566 <snapdealproject333@gmail.com>
2023-06-03 10:19:12 +0530

    testing

---
+++ src/main/java/com/snapdealProject/snapdeal/SnapdealApplication.java
@@ -0,0 +1,13 @@
+package com.snapdealProject.snapdeal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnapdealApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnapdealApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/snapdealProject/snapdeal/SnapdealApplicationTests.java
@@ -0,0 +1,13 @@
+package com.snapdealProject.snapdeal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnapdealApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
